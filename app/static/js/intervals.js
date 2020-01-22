938d3f14fec1181aa25f50980101e0d5e7a43731
Mateusz Fila <mf358630@okwf.fuw.edu.pl>
2020-01-22 21:47:09 +0100

    clean up

--- app/static/js/intervals.js
+++
@@ -1,8 +0,0 @@
-setInterval( function()
-{
-    $("#volume").load(location.href+" #volume>*","");
-
-    $("#stateButton").load(location.href+" #stateButton>*","");
-    $("#current").load(location.href+" #current>*","");
-    $("#next").load(location.href+" #next>*","");
-},1000);
